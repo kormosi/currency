510802b7cf6758da376b19e3eda1f6cc521ca7d8
Patrik Kormosi <pkormosi@frinx.io>
2022-07-25 22:31:46 +0200

    Get currencies from user, contact API

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn construct_currency_vector() -> Vec<String> {
+    let file = File::open("codes_unique_sorted.txt").expect("Error reading file");
+    let reader = BufReader::new(file);
+
+    // Some turbofish magic, I don't know
+    reader.lines().collect::<Result<_, _>>().unwrap()
+}
+
+mod user_input_processing {
+    use regex::Regex;
+    use std::io;
+
+    fn get_user_input() -> String {
+        println!("Enter currency pair:");
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Couldn't read line");
+
+        input.trim().to_uppercase().to_string()
+    }
+
+    fn is_input_in_valid_format(user_input: String) -> Option<(String, String)> {
+        let re = Regex::new(r"^(?P<cur1>[a-zA-Z]{3}) (?P<cur2>[a-zA-Z]{3})$")
+            .expect("Couldn't construct regex");
+
+        let captures = re.captures(&user_input)?;
+        Some((captures["cur1"].to_string(), captures["cur2"].to_string()))
+    }
+
+    /// A wrapper function around the above get_user_input() and
+    /// dissect_user_input() functions. Guaranteed to return a valid
+    /// currency pair (if one of the functions it calls doesn't panic).
+    pub fn get_valid_currency_codes() -> (String, String) {
+        let currency_vector = crate::construct_currency_vector();
+        loop {
+            let user_input = get_user_input();
+            if let Some(currencies) = is_input_in_valid_format(user_input) {
+                if !currency_vector.contains(&currencies.0)
+                    || !currency_vector.contains(&currencies.1)
+                {
+                    println!("Invalid currency code");
+                    continue;
+                }
+                return currencies;
+            } else {
+                println!("Invalid input format");
+                continue;
+            }
+        }
+    }
+}
+
+mod query_currency_api {
+    use std::env;
+
+    pub fn get_exchange_rate(cur1: String, cur2: String) {
+        let api_key =
+            env::var("CURRENCY_API_KEY").expect("CURRENCY_API_KEY environment variable not set");
+        let url = format!(
+            "https://free.currconv.com/api/v7/convert?q={}_{}&compact=ultra&apiKey={}",
+            cur1, cur2, api_key
+        );
+
+        // TODO change unwrap to at least expect
+        let resp = reqwest::blocking::get(url).unwrap().text().unwrap();
+        println!("{:#?}", resp);
+    }
+}
+
+pub fn run_app() {
+    let (cur1, cur2) = user_input_processing::get_valid_currency_codes();
+    query_currency_api::get_exchange_rate(cur1, cur2);
+}
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    currency::run_app();
+}
