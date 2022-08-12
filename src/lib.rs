22346e27f358ff9778e86210657fc4b6d697d2cc
Patrik Kormosi <pkormosi@frinx.io>
2022-08-12 19:42:54 +0200

    Refactor logic, add 'h' and 'q' commands

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::process;
+
+use user_input_processing::is_input_valid_currency_pair;
 
 fn construct_currency_vector() -> Vec<String> {
     let file = File::open("codes_unique_sorted.txt").expect("Error reading file");
@@ -13,18 +16,18 @@ mod user_input_processing {
     use regex::Regex;
     use std::io;
 
-    fn get_user_input() -> String {
-        println!("Enter currency pair:");
+    pub fn get_user_input() -> String {
+        println!("Enter currency pair. 'h' for history, 'q' to quit:");
         let mut input = String::new();
 
         io::stdin()
             .read_line(&mut input)
             .expect("Couldn't read line");
 
-        input.trim().to_uppercase()
+        input.trim().to_string()
     }
 
-    fn is_input_in_valid_format(user_input: String) -> Option<(String, String)> {
+    fn is_input_in_valid_format(user_input: &str) -> Option<(String, String)> {
         let re = Regex::new(r"^(?P<cur1>[a-zA-Z]{3}) (?P<cur2>[a-zA-Z]{3})$")
             .expect("Couldn't construct regex");
 
@@ -32,25 +35,22 @@ mod user_input_processing {
         Some((captures["cur1"].to_string(), captures["cur2"].to_string()))
     }
 
-    /// A wrapper function around the above `get_user_input`() and
-    /// `dissect_user_input`() functions. Guaranteed to return a valid
-    /// currency pair (if one of the functions it calls doesn't panic).
-    pub fn get_valid_currency_codes() -> (String, String) {
+    pub fn is_input_valid_currency_pair(user_input: &str) -> Option<(String, String)> {
+        // Vector of all valid currency codes
+        // TODO should be constructed only once
         let currency_vector = crate::construct_currency_vector();
-        loop {
-            let user_input = get_user_input();
-            if let Some(currencies) = is_input_in_valid_format(user_input) {
-                if !currency_vector.contains(&currencies.0)
-                    || !currency_vector.contains(&currencies.1)
-                {
-                    println!("Invalid currency code");
-                    continue;
-                }
-                return currencies;
-            } else {
-                println!("Invalid input format");
-                continue;
+
+        if let Some(currency_pair) = is_input_in_valid_format(user_input) {
+            if !currency_vector.contains(&currency_pair.0)
+                || !currency_vector.contains(&currency_pair.1)
+            {
+                println!("Invalid currency code");
+                return None;
             }
+            return Some(currency_pair);
+        } else {
+            println!("Invalid input format");
+            None
         }
     }
 }
@@ -62,10 +62,25 @@ mod price_operations {
     use std::collections::HashMap;
     use std::{env, error::Error};
 
+    pub fn print_exchange_rate(currency_pair: (String, String), yesterday_date: String) {
+        let exchange_rates_raw =
+            get_exchange_rate_raw(&currency_pair.0, &currency_pair.1, &yesterday_date).unwrap();
+
+        let (today_price, yesterday_price) = get_prices_from_api_response(
+            &currency_pair.0,
+            &currency_pair.1,
+            exchange_rates_raw,
+            &yesterday_date,
+        );
+
+        println!("today: {} .. yesterday: {}", today_price, yesterday_price);
+        compare_and_print_exchange_rate(today_price, yesterday_price);
+    }
+
     // Returns raw response from the API
     pub fn get_exchange_rate_raw(
-        cur1: String,
-        cur2: String,
+        cur1: &str,
+        cur2: &str,
         yesterday_date: &str,
     ) -> Result<(String, String), Box<dyn Error>> {
         let api_key =
@@ -89,19 +104,28 @@ mod price_operations {
 
     // TODO don't unwrap
     pub fn get_prices_from_api_response(
+        cur1: &str,
+        cur2: &str,
         exchange_rates_raw: (String, String),
         yesterday_date: &str,
     ) -> (f32, f32) {
-        // Convert the raw strings into hashmaps
+        // Convert the raw strings from the API response into hashmaps
         let today_price_map: HashMap<String, Value> =
             serde_json::from_str(&exchange_rates_raw.0).expect("JSON was not well-formatted");
         let yesterday_price_map: HashMap<String, Value> =
             serde_json::from_str(&exchange_rates_raw.1).expect("JSON was not well-formatted");
 
         // Get the numerical values of the exchange rates
-        let today_price = today_price_map.get("USD_CHF").unwrap().to_string();
+        // let today_price = today_price_map.get("USD_CHF").unwrap().to_string();
+        let formatted_currency_pair = format!("{}_{}", cur1, cur2);
+
+        let today_price = today_price_map
+            .get(&formatted_currency_pair)
+            .unwrap()
+            .to_string();
+
         let yesterday_price = yesterday_price_map
-            .get("USD_CHF")
+            .get(&formatted_currency_pair)
             .unwrap()
             .get(yesterday_date)
             .unwrap()
@@ -114,7 +138,7 @@ mod price_operations {
         (today_price, yesterday_price)
     }
 
-    pub fn print_colored_prices(today_price: f32, yesterday_price: f32) {
+    pub fn compare_and_print_exchange_rate(today_price: f32, yesterday_price: f32) {
         // Compare today's price against yesterday price, print colored output accordingly
         match today_price.partial_cmp(&yesterday_price) {
             Some(Ordering::Less) => println!("{}", today_price.to_string().red()),
@@ -171,24 +195,19 @@ mod sql_operations {
 // TODO this should return box dyn error.
 // That way all unwraps can be changed for ?
 pub fn run_app() {
-    let (cur1, cur2) = user_input_processing::get_valid_currency_codes();
-    // query_currency_api::get_exchange_rate(cur1, cur2);
-
-    // Get yesterday's date
-    let yesterday = chrono::Utc::now() - chrono::Duration::days(1);
-    let yesterday_formatted = yesterday.format("%Y-%m-%d").to_string();
-
-    // TODO don't unwrap
-    let exchange_rates_raw = price_operations::get_exchange_rate_raw(
-        "USD".to_string(),
-        "CHF".to_string(),
-        &yesterday_formatted,
-    )
-    .unwrap();
-
-    let (today_price, yesterday_price) =
-        price_operations::get_prices_from_api_response(exchange_rates_raw, &yesterday_formatted);
-
-    println!("today: {} .. yesterday: {}", today_price, yesterday_price);
-    price_operations::print_colored_prices(today_price, yesterday_price);
+    loop {
+        let user_input = user_input_processing::get_user_input();
+        if user_input == "h" {
+            sql_operations::get_history_from_db()
+        } else if user_input == "q" {
+            process::exit(0);
+        } else if let Some(currency_tuple) =
+            is_input_valid_currency_pair(&user_input.to_uppercase())
+        {
+            // Get yesterday's date and format it
+            let yesterday = chrono::Utc::now() - chrono::Duration::days(1);
+            let yesterday_formatted = yesterday.format("%Y-%m-%d").to_string();
+            price_operations::print_exchange_rate(currency_tuple, yesterday_formatted);
+        }
+    }
 }
